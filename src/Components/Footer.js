ff32e53680f7ff9e11421f4b7fb08f2ce03993f9
Muhammad Azhan Awais <57989015+AzhanAwais@users.noreply.github.com>
2020-11-21 01:04:14 +0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,12 +6,12 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class Footer extends Component{
     render(){
         return(
-            <div class="container-fluid container-10">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="div-title">
-                            <h5><span class="country">Other Countries </span> India - South Africa - Indonesia</h5>
-                            <h5><span class="country">Free Classifieds in Pakistan </span> © 2006-2020 OLX</h5>
+            <div className="container-fluid container-10">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="div-title">
+                            <h5><span className="country">Other Countries </span> India - South Africa - Indonesia</h5>
+                            <h5><span className="country">Free classifieds in Pakistan </span> © 2006-2020 OLX</h5>
                         </div>
                     </div>
                 </div>
